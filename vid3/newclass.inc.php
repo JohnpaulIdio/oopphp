431e8720202f90df4e6f004cc86a6246a5205360
johnpaul Idio <idiojohnpaul011@gmail.com>
2024-03-05 20:12:12 -0800

    first commit

---
+++ vid3/newclass.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+class NewClass {
+
+ //Properties and Methods goes here
+public $info = " lamig parang hotdog sa ref";
+
+}
+
+$object = new NewClass;
+var_dump($object);
---
+++ vid4/person.inc01.php
@@ -0,0 +1,21 @@
+<?php
+
+class Person {
+    private $first = "Johnpaul";
+    private $last = "Idio";
+    private $age = "20";
+    private $occupation = "Engineer";
+
+public function owner() {
+
+    $a = $this->first. 
+    return $a;
+}
+}
+class pet {
+//     public function owner() {
+//         $a = "Pagod na ako!";
+//         return $a;
+       
+//  }
+}
